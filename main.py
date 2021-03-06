864d5a28fe164d0107c63c4cdd9e0e75f20ed1bf
Spalesta <357868721022006Spalesta>
2021-03-06 20:19:21 +0300

    реализовано движение одного шарика с помощью методов класса Ball. шарик отталкивается от краев окошка. левой кнопкой мыши шарик останавлвается, правой - продолжает движение. нет рандомной скорости и рандомного цвета. на данный момент в классе Ball методы init, hmove и redraw.

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
 
-class Ball():
+
+class Ball:
     def __init__(self, pos, color):
         self.pos = pos
         self.right = True
         self.color = color
         self.stop = False
         pygame.draw.circle(screen, self.color, self.pos, 50)
-    def move(self):
-        self.pos = (self.pos[0] + 1, self.pos[1])
-        if not self.stop:
-            if self.pos[0] == 550:
-                self.right = False
-            if self.pos[0] == 50:
-                self.right = True
-            if self.right:
-                self.pos = (self.pos[0], self.pos[1])
-            else:
-                self.pos = (self.pos[0], self.pos[1])
+
+    def hmove(self):
+        if self.pos[0] == 550:
+            self.right = False
+        if self.pos[0] == 50:
+            self.right = True
+        if self.right:
+            self.pos = (self.pos[0] + 1, self.pos[1])
+        else:
+            self.pos = (self.pos[0] - 1, self.pos[1])
+
     def redraw(self):
         pygame.draw.circle(screen, self.color, self.pos, 50)
 
@@ -25,7 +26,6 @@ class Ball():
 pygame.init()
 
 screen = pygame.display.set_mode((600, 300))
-screen.fill((112, 146, 190))
 x, y = 100, 100
 right = True
 running = True
@@ -34,7 +34,7 @@ clock = pygame.time.Clock()
 circle_color = (236, 176, 199)
 screen_color = (112, 146, 190)
 stop = False
-ball = Ball((x, y), circle_color)
+first_ball = Ball((x, y), circle_color)
 
 while running:
     for event in pygame.event.get():
@@ -45,15 +45,10 @@ while running:
                 stop = True
             else:
                 stop = False
-        '''if event.type == pygame.MOUSEBUTTONUP:
-            stop = False'''
+    if not stop:
+        first_ball.hmove()
+    first_ball.redraw()
     pygame.display.flip()
     screen.fill((112, 146, 190))
-
-    ball.redraw()
-
-    ball.move()
-    ball.redraw()
     clock.tick(FPS)
-
 pygame.quit()

a4297927273739f8b147253f48339d38cd24759f
Spalesta <357868721022006Spalesta>
2021-03-06 21:34:11 +0300

    ТЕПЕРЬ ОНИ ДВИГАЮТСЯ ПО Y ТОЖЕ!!!!!!! и отталкиваются!!!!!!!!!!!

--- main.py
+++ main.py
@@ -6,20 +6,32 @@ class Ball:
     def __init__(self, pos):
         self.pos = pos
         self.right = True
+        self.up = True
         self.color = (random.randint(100, 255), random.randint(100, 255), random.randint(0, 200))
         self.stop = False
-        self.speed = random.randint(1, 3)
+        self.xspeed = random.randint(1, 3)
+        self.yspeed = random.randint(1, 3)
         pygame.draw.circle(screen, self.color, self.pos, 50)
 
-    def hmove(self):
+    def ymove(self):
+        if self.pos[1] >= y_limit - radius:
+            self.up = True
+        if self.pos[1] <= radius:
+            self.up = False
+        if self.up:
+            self.pos = (self.pos[0], self.pos[1] - self.yspeed)
+        else:
+            self.pos = (self.pos[0], self.pos[1] + self.yspeed)
+
+    def xmove(self):
         if self.pos[0] >= 550:
             self.right = False
         if self.pos[0] <= 50:
             self.right = True
         if self.right:
-            self.pos = (self.pos[0] + self.speed, self.pos[1])
+            self.pos = (self.pos[0] + self.xspeed, self.pos[1])
         else:
-            self.pos = (self.pos[0] - self.speed, self.pos[1])
+            self.pos = (self.pos[0] - self.xspeed, self.pos[1])
 
     def redraw(self):
         pygame.draw.circle(screen, self.color, self.pos, 50)
@@ -27,6 +39,9 @@ class Ball:
 
 pygame.init()
 
+radius = 50
+y_limit = 300
+x_limit = 600
 screen = pygame.display.set_mode((600, 300))
 running = True
 FPS = 150
@@ -47,7 +62,8 @@ while running:
                 if balls:
                     del balls[0]
     for ball in balls:
-        ball.hmove()
+        ball.xmove()
+        ball.ymove()
         ball.redraw()
     pygame.display.flip()
     screen.fill((112, 146, 190))

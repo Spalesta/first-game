f9b2882378bddb629608a41806efb275ff4d5cec
Splaesta <spalesta@gmail.com>
2021-03-15 17:26:17 +0300

    они закрашиваются и открашиваются при клике мышки

--- board.py
+++ board.py
@@ -9,12 +9,24 @@ class Board:
         self.width = width
         self.height = height
         self.cell_size = 30
+        self.matrix = [[0] * (self.height // self.cell_size) for _ in range(self.width // self.cell_size)]
 
     def redraw(self):
         x, y = 0, 0
         for i in range(self.height // self.cell_size):
             for j in range(self.width // self.cell_size):
-                pygame.draw.rect(screen, (200, 200, 200), (i * self.cell_size, j * self.cell_size, self.cell_size, self.cell_size), 1)
+                if not self.matrix[i][j]:
+                    pygame.draw.rect(screen, (200, 200, 200), (i * self.cell_size, j * self.cell_size, self.cell_size, self.cell_size), 1)
+                else:
+                    pygame.draw.rect(screen, (200, 200, 200),(i * self.cell_size, j * self.cell_size, self.cell_size, self.cell_size))
+
+    def paint(self, pos):
+        x, y = pos
+        self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size] = 1
+
+    def not_paint(self, pos):
+        x, y = pos
+        self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size] = 0
 
 
 board = Board(1000, 1000)
@@ -23,6 +35,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = event.pos
+                board.paint(pos)
+            elif event.button == 3:
+                pos = event.pos
+                board.not_paint(pos)
     screen.fill(screen_color)
     board.redraw()
 

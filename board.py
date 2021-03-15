9f239969fe1b2253db4cbe2e4ebd3510a4db8178
Splaesta <spalesta@gmail.com>
2021-03-15 17:30:13 +0300

    небольшие изменения

--- board.py
+++ board.py
@@ -9,7 +9,7 @@ class Board:
         self.width = width
         self.height = height
         self.cell_size = 30
-        self.matrix = [[0] * (self.height // self.cell_size) for _ in range(self.width // self.cell_size)]
+        self.matrix = [[False] * (self.height // self.cell_size) for _ in range(self.width // self.cell_size)]
 
     def redraw(self):
         x, y = 0, 0
@@ -20,13 +20,9 @@ class Board:
                 else:
                     pygame.draw.rect(screen, (200, 200, 200),(i * self.cell_size, j * self.cell_size, self.cell_size, self.cell_size))
 
-    def paint(self, pos):
+    def click(self, pos):
         x, y = pos
-        self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size] = 1
-
-    def not_paint(self, pos):
-        x, y = pos
-        self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size] = 0
+        self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size] = not(self.matrix[pos[0] // self.cell_size][pos[1] // self.cell_size])
 
 
 board = Board(1000, 1000)
@@ -38,10 +34,8 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 pos = event.pos
-                board.paint(pos)
-            elif event.button == 3:
-                pos = event.pos
-                board.not_paint(pos)
+                board.click(pos)
+
     screen.fill(screen_color)
     board.redraw()
 
